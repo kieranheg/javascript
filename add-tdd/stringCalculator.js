a11dc64523ed3c0cdc078e8bd5c3c97c2d57ed81
Kieran Hegarty <kieranheg@users.noreply.github.com>
2018-03-28 20:55:42 +0100

    Fix formatting on return

--- add-tdd/stringCalculator.js
+++ add-tdd/stringCalculator.js
@@ -3,11 +3,6 @@
 module.exports = {
   add: function (strList) {
     var delimiter = strList.substr(0, 2) === '//' ? strList.substr(2, 1) : ',';
-    return
-    strList.replace('//', '')
-      .replace(/\n/g, delimiter)
-      .split(delimiter)
-      .map(Number)
-      .reduce((sum, value) => sum + value);
+    return strList.replace('//', '').replace(/\n/g, delimiter).split(delimiter).map(Number).reduce((sum, value) => sum + value);
   }
 }
--- add-tdd/test/stringCalculator_test.js
+++ add-tdd/test/stringCalculator_test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const expect = require('chai').expect;
-const stringCalculator = require('./stringCalculator');
+const stringCalculator = require('../stringCalculator');
 
 describe("String Calculator tests", function () {
   describe("Add", function () {
