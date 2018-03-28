aaa97da8984911bfa2eee269094a5c2a8ac6dfd0
Kieran Hegarty <kieranheg@users.noreply.github.com>
2018-03-28 20:50:11 +0100

    Fix formatting

--- add-tdd/stringCalculator.js
+++ add-tdd/stringCalculator.js
@@ -1,8 +1,13 @@
 "use strict";
 
 module.exports = {
-   add: function(strList) {
-       var delimiter = strList.substr(0,2) === '//' ? strList.substr(2,1) : ',';
-       return strList.replace('//', '').replace(/\n/g, delimiter).split(delimiter).map(Number).reduce((sum, value) => sum + value);
-   }
+  add: function (strList) {
+    var delimiter = strList.substr(0, 2) === '//' ? strList.substr(2, 1) : ',';
+    return
+    strList.replace('//', '')
+      .replace(/\n/g, delimiter)
+      .split(delimiter)
+      .map(Number)
+      .reduce((sum, value) => sum + value);
+  }
 }
