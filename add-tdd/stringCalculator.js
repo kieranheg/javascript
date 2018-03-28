b25ecd40d33ff9820c952bd62b671db557516bee
Kieran Hegarty <kieranheg@users.noreply.github.com>
2018-03-28 21:47:56 +0100

    refector add

--- add-tdd/stringCalculator.js
+++ add-tdd/stringCalculator.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  add: function (strList) {
+  add: (strList) => {
     var delimiter = strList.substr(0, 2) === '//' ? strList.substr(2, 1) : ',';
     return strList.replace('//', '').replace(/\n/g, delimiter).split(delimiter).map(Number).reduce((sum, value) => sum + value);
   }
